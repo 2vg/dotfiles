a2d616c9d844b6335ee37e2f84636862c11470ef
momf <m0fqn090310@gmail.com>
2017-10-22 22:52:19 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -81,14 +81,21 @@ else
 fi
 
 if $nvim_exists; then
+  if [ ! -d ~/.nvim ]; then
+    mkdir -p ~/.nvim/plugin
+  fi
   ln -s ~/src/dotfiles/.nvim/init.vim ~/.config/nvim/init.vim
   ln -s ~/src/dotfiles/.nvim/plugin/ ~/.nvim/plugin
 else
+  if [ ! -d ~/.vim ]; then
+    mkdir -p ~/.vim/plugin
+  fi
   ln -s ~/src/dotfiles/.vim/.vimrc ~/.vimrc
   ln -s ~/src/dotfiles/.vim/plugin/ ~/.vim/plugin
 fi
 
 if [ ! -d ~/.zplug ];$ then
+  mkdir -p ~/.zplug
   curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 fi
 
